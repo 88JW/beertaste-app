6ea78d92779bd160bf1675f413732a0a92ac2d13
Wojciech Jaskula <w.jaskula8@gmail.com>
2025-02-14 12:24:57 +0100

    strrona logowania logo + fiox kolorówv

--- src/components/auth/WelcomePage.jsx
+++ src/components/auth/WelcomePage.jsx
@@ -4,25 +4,43 @@ import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
+import btaLogo from '../../assets/bta_logo.png';
 
 function WelcomePage({ user }) {
   return (
-    <Container
-    >
-      <Typography variant="h1" align="center" gutterBottom>Witaj!</Typography>
+    <Container>
+      {/* <Typography variant="h4" align="center" gutterBottom>Witaj!</Typography> */}
+      <img src={btaLogo} alt="BTA Logo" style={{ display: 'block', margin: '0 auto', maxWidth: '250px' }} />
       {user ? (
         <Typography align="center">Jesteś zalogowany jako: {user.email}</Typography>
       ) : null}
       <Stack direction="column" spacing={2} sx={{ mt: 2 }}>
-        <Button component={Link} to="/login" color="primary" variant="contained" >Zaloguj się</Button>
-        <Button component={Link} to="/register" color="primary" variant="contained" >Zarejestruj</Button>
+        <Button
+          component={Link}
+          to="/login"
+          variant="contained"
+          sx={{
+            backgroundColor: '#959B27',
+            border: '2px solid black',
+            '&:hover': { backgroundColor: '#FFCF54' } // dodano hover kolor
+          }}
+        >
+          Zaloguj się
+        </Button>
+        <Button
+          component={Link}
+          to="/register"
+          variant="contained"
+          sx={{
+            backgroundColor: '#959B27',
+            border: '2px solid black',
+            '&:hover': { backgroundColor: '#FFCF54' } // dodano hover kolor
+          }}
+        >
+          Zarejestruj
+        </Button>
       </Stack>
     </Container>
   );
 }
-
-// Analiza: 
-// - Wyświetla tytuł powitalny i (jeśli dostępny) email użytkownika.
-// - Zawiera przyciski nawigacyjne do stron logowania i rejestracji.
-
 export default WelcomePage;
